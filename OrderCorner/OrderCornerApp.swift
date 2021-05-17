1e6a00209887f67b96d98422379b0acc1b55eb30
Antonio Massa <antonio.massa@zehus.it>
2021-05-17 10:34:51 +0200

    Initial Commit

---
+++ OrderCorner/OrderCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderCornerApp.swift
+//  OrderCorner
+//
+//  Created by Massa Antonio on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct OrderCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
