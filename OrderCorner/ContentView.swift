b2df171d67bc2e245de5686721c2c8b393619a5b
Antonio Massa <antonio.massa@zehus.it>
2021-06-03 14:13:51 +0200

    Testing the app

--- OrderCorner/ContentView.swift
+++ OrderCorner/ContentView.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var order = Order()
+    @State private var username = ""
+    @State private var email = ""
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //        Form{
+        //            Section{
+        //                TextField("Username", text: $username)
+        //                TextField("Email", text: $email)
+        //            }
+        //
+        //            Section {
+        //                Button("Create account") {
+        //                    print("Creating account")
+        //                }
+        //            }
+        //            .disabled(disableForm)
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    Stepper(value: $order.quantity, in: 1...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
